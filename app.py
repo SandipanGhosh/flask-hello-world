ed712980455fd4f0a3a89132f1786d4f0a95edfd
SandipanGhosh <sandipan.ghosh2005@gmail.com>
2018-01-19 11:48:12 -0500

    Added dynamic routes with explicit status codes. Added debug mode.

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ from flask import Flask
 # create the application object
 app = Flask(__name__)
 
+# error handling
+app.config["DEBUG"] = True
+
 # use the decorator pattern to link the view function to a url
 @app.route("/")
 @app.route("/hello")
@@ -14,9 +17,33 @@ app = Flask(__name__)
 def hello_world():
 	return "Hello, World!"
 
+# dynamic route
 @app.route("/test/<search_query>")
 def search(search_query):
 	return search_query
+
+@app.route("/integer/<int:value>")
+def int_type(value):
+	print(value + 1)
+	return "correct"
+
+@app.route("/float/<float:value>")
+def float_type(value):
+	print(value + 1)
+	return "correct"
+
+@app.route("/path/<path:value>")
+def path_value(value):
+	print(value)
+	return "correct"
+
+# dynamic route with explicit status codes
+@app.route("/name/<name>")
+def index(name):
+	if name.lower() == "sandipan":
+		return "Hello {}".format(name)
+	else:
+		return "Not found", 404
 
 # start the development server using the run() method
 if __name__ == "__main__":
